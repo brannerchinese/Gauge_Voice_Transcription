8f75a34d7cf60cdd2d646c72ad52052243cf607d
David Branner <brannerchinese@gmail.com>
2014-01-30 15:37:10 -0500

    update py.test

--- test/test_gauge_voice_transcription_py.py
+++ test/test_gauge_voice_transcription_py.py
@@ -5,9 +5,12 @@
 import pytest
 import gauge_voice_transcription_py2 as G
 
+s1 = 'abc'
+
 def simple01():
-    s1 = 'abc'
     s2 = 'abc'
-    assert 1.0, 1.0, 1.0 == G.percent_matching(s1, s2)
+    assert (1.0, 1.0, 1.0) == G.percent_matching(s1, s2)
 
-
+def simple02():
+    s2 = 'abc '
+    assert (0.8571428571428571, 1.0, 1.0) == G.percent_matching(s1, s2)
