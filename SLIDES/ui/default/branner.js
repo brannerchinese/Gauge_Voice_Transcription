c43046db0aac28c8674f8f3442f096ce7b5bedee
David Branner <brannerchinese@gmail.com>
2014-02-15 14:15:51 -0500

    Update branner.js

--- SLIDES/ui/default/branner.js
+++ SLIDES/ui/default/branner.js
@@ -1,11 +1,11 @@
 // David Prager Branner
 // 20140215
 
-// Different settings if running locally or on web.
+// Different settings for JS and CSS if running locally or on web.
 var currentURL = window.location.href;
 if (currentURL.search(/http/) === 0) {
   // Don't use htmlpreview.github.io here; displays markdown, not HTML.
-  var urlPrefix = "https://github.com/brannerchinese/notes/blob/master/";
+  var urlPrefix = "https://github.com/brannerchinese/Gauge_Voice_Transcription/blob/master/SLIDES/";
   var whereWeAre = "via HTTP.";
 }
 else {
